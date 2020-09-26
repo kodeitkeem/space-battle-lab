a0c5427c315357388e1a697b32c78ac43e024b89
darrylt <4dtait@gmail.com>
2020-09-26 12:37:36 -0600

    update

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 // Run prompt to get user input
 // Use input to call function or something similar to attack ship
 
-
-let userInput = ''
+let userInput = "";
 
 const USSAssembly = {
-    hull: 20,
-    firepower: 5,
-    accuracy: .7,
-    attack: function(alienShip1) {
-        userInput = prompt('Do you want to attack alien ship? Enter "yes" to continue')
-        if (userInput === 'yes') {
-            let shot = Math.random()
-            if (shot < this.accuracy) {
-                alienShip1.hull -= this.firepower
-            }
-        }
+  hull: 20,
+  firepower: 5,
+  accuracy: 0.7,
+  attack: function (alienShip1) {
+    userInput = prompt(
+      'Do you want to attack alien ship? Enter "yes" to continue'
+    );
+    if (userInput === "yes") {
+      let shot = Math.random();
+      if (shot < this.accuracy) {
+        alienShip1.hull -= this.firepower;
+      }
     }
-}
+  },
+};
 
 const alienShip1 = {
-    hull: 10,
-    firepower: Math.floor(Math.random() * 3) + 2,
-    accuracy: Math.random() * (.8 - .6) + .6,
-    }
+  hull: 10,
+  firepower: Math.floor(Math.random() * 3) + 2,
+  accuracy: Math.random() * (0.8 - 0.6) + 0.6,
+};
 
-USSAssembly.attack(alienShip1)
-console.log(alienShip1.hull)
-
-
-
-
-
-
-
-
+USSAssembly.attack(alienShip1);
+console.log(alienShip1.hull);
